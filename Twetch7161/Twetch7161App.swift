5a2d406761a56beb5e2cfcbccda3d74f6746aaba
Henry Hudson <henryhudson1431@yahoo.com>
2020-07-01 02:37:48 +0100

    Initial Commit

---
+++ Twetch7161/Twetch7161App.swift
@@ -0,0 +1,17 @@
+//
+//  Twetch7161App.swift
+//  Twetch7161
+//
+//  Created by Henry Hudson on 01/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct Twetch7161App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Twetch7161Tests/Twetch7161Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Twetch7161Tests.swift
+//  Twetch7161Tests
+//
+//  Created by Henry Hudson on 01/07/2020.
+//
+
+import XCTest
+@testable import Twetch7161
+
+class Twetch7161Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
